4dcc5a2c227a681fff68ebe62d60c3b31d82a837
dea-project07x <deakun23@gmail.com>
2026-09-19 12:32:58 +0900

    Create products.php

---
+++ products.php
@@ -0,0 +1,9 @@
+<?php
+// Data Layer - products.php
+$products = [
+    ["id" => 1, "name" => "Laptop Asus ROG", "price" => 15000000, "stock" => 5, "category" => "Laptop"],
+    ["id" => 2, "name" => "Mouse Logitech", "price" => 350000, "stock" => 20, "category" => "Aksesoris"],
+    ["id" => 3, "name" => "Keyboard Mechanical", "price" => 750000, "stock" => 15, "category" => "Aksesoris"],
+    ["id" => 4, "name" => "Monitor LG 24 inch", "price" => 2000000, "stock" => 8, "category" => "Monitor"]
+];
+?>
